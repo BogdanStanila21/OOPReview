91866e64f73a2c817fa0dbb8f269f0e90e972841
Bogdan <bogdanstanila2190@gmail.com>
2020-01-29 12:31:40 +0100

    Ejercicios clases

---
+++ Ejercicio1.ts
@@ -0,0 +1,23 @@
+export class Mobile {
+    name : string;
+    model : string;
+    trademark : string;
+    sdSize: number;
+    color: string;
+    is5G: boolean;
+    cameraNumber: number;
+    price: number;
+
+    constructor (name, model, trademark,sdSize, color, is5G,cameraNumber,price){
+        this.name= name;
+        this.model=model;
+        this.trademark=trademark;
+        this.sdSize= sdSize;
+        this.color= color;
+        this.is5G= is5G;
+        this.cameraNumber= cameraNumber;
+        this.price=price;
+    }
+
+
+}
---
+++ Ejercicio2.ts
@@ -0,0 +1,69 @@
+import {Mobile} from "./Ejercicio1";
+
+var Nokia3210:Mobile;
+Nokia3210 = new Mobile("Nokia3210","3210","Nokia",4,"red",false,0,100);
+
+var iPhone3G:Mobile;
+iPhone3G = new Mobile("iPhone3G","3G","Apple",32,"White",false,2,500);
+
+var SamsungS10:Mobile;
+SamsungS10= new Mobile("SamsungS10,","S10","Samsung",256,"Black",true,4,1100)
+
+
+console.log(Nokia3210.name)
+console.log(Nokia3210.model)
+console.log(Nokia3210.trademark)
+console.log(Nokia3210.sdSize)
+console.log(Nokia3210.color)
+console.log(Nokia3210.is5G)
+console.log(Nokia3210.cameraNumber)
+console.log(Nokia3210.price)
+
+console.log(iPhone3G.name)
+console.log(iPhone3G.model)
+console.log(iPhone3G.trademark)
+console.log(iPhone3G.sdSize)
+console.log(iPhone3G.color)
+console.log(iPhone3G.is5G)
+console.log(iPhone3G.cameraNumber)
+console.log(iPhone3G.price)
+
+console.log(SamsungS10.name)
+console.log(SamsungS10.model)
+console.log(SamsungS10.trademark)
+console.log(SamsungS10.sdSize)
+console.log(SamsungS10.color)
+console.log(SamsungS10.is5G)
+console.log(SamsungS10.cameraNumber)
+console.log(SamsungS10.price)
+
+Nokia3210.is5G=true;
+Nokia3210.cameraNumber = 4;
+
+console.log(Nokia3210.name)
+console.log(Nokia3210.model)
+console.log(Nokia3210.trademark)
+console.log(Nokia3210.sdSize)
+console.log(Nokia3210.color)
+console.log(Nokia3210.is5G)
+console.log(Nokia3210.cameraNumber)
+console.log(Nokia3210.price)
+
+console.log(iPhone3G.name)
+console.log(iPhone3G.model)
+console.log(iPhone3G.trademark)
+console.log(iPhone3G.sdSize)
+console.log(iPhone3G.color)
+console.log(iPhone3G.is5G)
+console.log(iPhone3G.cameraNumber)
+console.log(iPhone3G.price)
+
+console.log(SamsungS10.name)
+console.log(SamsungS10.model)
+console.log(SamsungS10.trademark)
+console.log(SamsungS10.sdSize)
+console.log(SamsungS10.color)
+console.log(SamsungS10.is5G)
+console.log(SamsungS10.cameraNumber)
+console.log(SamsungS10.price)
+
